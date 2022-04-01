1876564db64b7d512b749cf4decd1257ceb8cc9a
Konstantin Romanov <kromanov1@bloomberg.net>
2022-03-31 23:09:18 -0400

    Add simple driver for parsing.

--- src/tipc.ml
+++ src/tipc.ml
@@ -1,3 +1,17 @@
 (* Driver for TIP parser *)
 
-let () = Printf.printf "Hello world!\n"
+let parse filename =
+  let lexbuf = Lexing.from_channel (open_in filename) in
+
+  try
+    let program = Parser.program Lexer.token lexbuf in
+    List.iter (Printf.printf "%d\n%!") program
+  with
+  | End_of_file -> Printf.printf "end of file"
+  | _ -> Printf.printf "Unknown error while parsing %s\n" filename
+
+let _ =
+  match Sys.argv with
+  | [| _ |] -> Printf.eprintf "Please provide TIP file to parse.\n"
+  | [| _; filename |] -> parse filename
+  | _ -> Printf.eprintf "Please provide only one file to parse."
