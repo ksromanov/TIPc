fd1723247171226c9a058a115aabc30d9563fe65
Konstantin Romanov <kromanov1@bloomberg.net>
2022-07-26 00:06:08 -0400

    Add joins.

--- src/interval/interval.ml
+++ src/interval/interval.ml
@@ -1,22 +1,61 @@
 (* Interval analysis using widening and narrowing, without delta operator. *)
 type value = Value of int | Neg_infty | Pos_infty [@@deriving show]
+
+let less = function
+  | _, Neg_infty -> false
+  | Neg_infty, _ -> true
+  | Pos_infty, _ -> false
+  | _, Pos_infty -> true
+  | Value a, Value b -> a < b
+
 type interval = value * value [@@deriving show]
 type interval_lattice_t = Bottom | Interval of interval [@@deriving show]
 
 (* Top is encoded as an interval to avoid duplication *)
 let top = Interval (Neg_infty, Pos_infty)
-let is_top = (=) top
+let is_top = ( = ) top
+
+(* Simple join on the lattice values *)
+let join a b =
+  let join_intervals (a1, a2) (b1, b2) =
+    let min a b = if less (a, b) then a else b in
+    let max a b = if less (a, b) then b else a in
+    (min a1 b1, max a2 b2)
+  in
+  match (a, b) with
+  | Bottom, _ -> b
+  | _, Bottom -> a
+  | Interval a, Interval b -> Interval (join_intervals a b)
 
 module VarIntervalHashtbl = struct
   type t = (string, interval_lattice_t) Hashtbl.t
 
   let pp ppf values =
-    Hashtbl.iter (fun key value ->
-      Format.fprintf ppf "@[<1>%s: %s@]@." key (show_interval_lattice_t value)) values
+    Hashtbl.iter
+      (fun key value ->
+        Format.fprintf ppf "@[<1>%s: %s@]@." key (show_interval_lattice_t value))
+      values
+
+  let join
+      (merge : interval_lattice_t -> interval_lattice_t -> interval_lattice_t)
+      (a : t) (b : t) =
+    let result = Hashtbl.copy a in
+    let _ =
+      Hashtbl.iter
+        (fun k vb ->
+          match Hashtbl.find_opt result k with
+          | None -> ()
+          | Some va -> Hashtbl.replace result k (merge va vb))
+        b
+    in
+    result
 end
 
 (* The lattice for a single statement, state_t in other words. *)
-type payload = VarIntervalHashtbl [@@deriving show]
+type payload = VarIntervalHashtbl.t [@@deriving show]
+
+(* join function for the lattice *)
+let join_states (a : payload) (b : payload) = VarIntervalHashtbl.join join a b
 
 type statement =
   | Assignment of Anf.ident * Anf.expression * payload
@@ -33,18 +72,21 @@ type statement =
 [@@deriving show]
 
 (* Add default payload to the tree *)
-let rec preset_statement (payload:payload)  =
-  function
-  | Anf.Assignment (id, expr) -> Assignment (id, expr, payload)
-  | Anf.Output expr -> Output (expr, payload)
-  | Anf.Error expr -> Error (expr, payload)
+let rec preset_statement (payload : unit -> payload) = function
+  | Anf.Assignment (id, expr) -> Assignment (id, expr, payload ())
+  | Anf.Output expr -> Output (expr, payload ())
+  | Anf.Error expr -> Error (expr, payload ())
   | Anf.If (cond, thn, els) ->
-      If (cond, preset_statement payload thn, Option.map (preset_statement payload ) els, payload)
+      If
+        ( cond,
+          preset_statement payload thn,
+          Option.map (preset_statement payload) els,
+          payload () )
   | Anf.While (cond, body) ->
-      While (cond, List.map (preset_statement payload) body, payload)
-  | Anf.Store (id, expr) -> Store (id, expr, payload)
+      While (cond, List.map (preset_statement payload) body, payload ())
+  | Anf.Store (id, expr) -> Store (id, expr, payload ())
   | Anf.DirectRecordWrite (r, field, expr) ->
-      DirectRecordWrite (r, field, expr, payload)
+      DirectRecordWrite (r, field, expr, payload ())
   | Anf.Block body -> Block (List.map (preset_statement payload) body)
 
 (* Create an approximation to currently available expressions
@@ -61,20 +103,34 @@ let analyze_function
   let rec analyze_statement previous_state = function
     (* remove all expressions, which contain the id *)
     | Assignment (id, expr, _) -> failwith "Interval: unimplemented"
-    | Output (atomic_expr, _) -> failwith "Interval: unimplemented"
-    | Error (atomic_expr, _) -> failwith "Interval: unimplemented"
-    | If (cond, thn, els, _) -> failwith "Interval: unimplemented"
+    | Output (atomic_expr, _) ->
+        (previous_state, Output (atomic_expr, Hashtbl.copy previous_state))
+    | Error (atomic_expr, _) ->
+        (previous_state, Error (atomic_expr, Hashtbl.copy previous_state))
+    | If (cond, thn, els, _) -> (
+        let thn_state, thn_result =
+          analyze_statement (Hashtbl.copy previous_state) thn
+        in
+        match els with
+        | None -> (thn_state, If (cond, thn, None, Hashtbl.copy previous_state))
+        | Some els ->
+            let els_state, els_result =
+              analyze_statement (Hashtbl.copy previous_state) els
+            in
+            ( join_states thn_state els_state,
+              If (cond, thn, Some els, Hashtbl.copy previous_state) ))
     | While (cond, body, _) -> failwith "Interval: unimplemented"
-    | Store (id, expr, _) -> failwith "Interval: unimplemented"
-    | DirectRecordWrite (r, field, expr, _) ->
-        failwith "Interval: unimplemented"
+    | Store (_, _, _) as stmt -> (previous_state, stmt) (* unimplemented *)
+    | DirectRecordWrite (_, _, _, _) as stmt ->
+        (previous_state, stmt) (* unimplemented *)
     | Block body ->
         let state, body =
           List.fold_left_map analyze_statement previous_state body
         in
         (state, Block body)
   in
-  let _ = List.fold_left_map analyze_statement () (failwith "" (*stmts*)) in
+  let stmts = List.map (preset_statement (fun () -> Hashtbl.create 10)) stmts in
+  let _ = List.fold_left_map analyze_statement (Hashtbl.create 10) stmts in
   ()
 
 (* Main entry-point of the pass *)
